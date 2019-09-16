02599d420f8e5fc59919ed8192a0395627c9c692
virtuotechs <virtuotechs@gmail.com>
2019-09-16 13:23:28 +0530

    responsive

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "24c7fa68196986e4327d";
+/******/ 	var hotCurrentHash = "94049b57a51ed866d291";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
